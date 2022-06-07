f6a9b6e9a8b5edeabf614d735fc857f495da33f4
Ben Zonneveld <whilecount@gmail.com>
2022-06-07 03:28:53 +0200

    Implementing MIDI to MiniDexed. Need to patch MiniDexed to add some custom sysex.

--- MiniDexed_Master/MDMA.h
+++ MiniDexed_Master/MDMA.h
@@ -3,12 +3,10 @@
 #include "Dexed/tg.h"
 #include "pico/util/queue.h"
 
-#define FIFOLENGTH 8
+#define FIFOLENGTH 16
 
 extern queue_t tg_fifo;
 extern queue_t midi_fifo;
-
-#define FIFOLENGTH 16
 
 extern cPots Pots;
 extern cButtons buttons;
